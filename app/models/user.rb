5ba6c53d4a4c5de89683d9c4541532720a79bffb
M416 <chanon_w@cmu.ac.th>
2018-07-02 14:30:55 +0700

    complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,39 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :organization
   has_many :project_users
+  
+  validates :email, :presence => true
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+  
+  rails_admin do
+  
+  	list do
+  		field :id
+  		field :first_name
+  		field :last_name
+  		field :email
+      field :organization
+
+  	end
+
+  	edit do
+  		field :first_name
+  		field :last_name
+  		field :email
+  		field :password
+  		field :password_confirmation
+      field :organization
+
+  	end
+  end
+
+  RailsAdmin.config do |config|
+    config.model 'organization' do
+      show do
+        field :name
+      end
+    end
+  end
+
 end
